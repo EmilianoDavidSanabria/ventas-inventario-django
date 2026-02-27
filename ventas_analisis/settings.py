286e72ced558d03e0459c6ac70b1be2a76d1ea0e
EmilianoDavidSanabria <emilianods05@gmail.com>
2026-02-27 12:27:46 -0300

    Configurar ALLOWED_HOSTS

--- ventas_analisis/settings.py
+++ ventas_analisis/settings.py
@@ -5,13 +5,13 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-xf8hqe7@nk4+ni4e@ru52c^desyl!plwawwips@dd5sn4eytg#')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']  # Agrega tus dominios en producción aquí
+ALLOWED_HOSTS = [
+    '*'
+]
 
 # Application definition
 
